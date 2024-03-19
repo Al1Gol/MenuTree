3d28cc648c752eb56861217937868ab7628f9ac5
KorepanovA <al1working@mail.ru>
2024-03-19 21:41:11 +0300

    Создание проекта, добавление основных моделей

---
+++ MenuTree/mainapp/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Список меню
+class Menus(models.Model):
+    name = models.CharField(verbose_name="Наименование меню", max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+# Первый уровень вложенности
+class Nesting_1(models.Model):
+    name = models.CharField(verbose_name="Наименование превого пунка вложенности", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Второй уровень вложенности
+class Nesting_2(models.Model):
+    name = models.CharField(verbose_name="Наименование второго пунка вложенности", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Третий уровень вложенности
+class Nesting_2(models.Model):
+    name = models.CharField(verbose_name="Наименование 3 пунка вложенности", max_length=100)
+
+    def __str__(self):
+        return self.name
