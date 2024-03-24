f108141efa614fc9710c3e7837beab17554ffd35
KorepanovA <al1working@mail.ru>
2024-03-24 03:33:24 +0300

    Подключена статика. Переработана структура моделей

--- MenuTree/mainapp/admin.py
+++ MenuTree/mainapp/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Menus, Nesting_1, Nesting_2, Nesting_3
+
+from .models import Menus
 
 admin.site.register(Menus)
-admin.site.register(Nesting_1)
-admin.site.register(Nesting_2)
-admin.site.register(Nesting_3)
--- MenuTree/mainapp/models.py
+++ MenuTree/mainapp/models.py
@@ -1,30 +1,37 @@
 from django.db import models
 
+
 # Список меню
 class Menus(models.Model):
     name = models.CharField(verbose_name="Наименование меню", max_length=100)
 
-    def __str__(self):
-        return self.name
-    
-
-# Первый уровень вложенности
-class Nesting_1(models.Model):
-    name = models.CharField(verbose_name="Наименование превого пунка вложенности", max_length=100)
-
+    # Настройка отображения записи в админке
     def __str__(self):
         return self.name
 
-# Второй уровень вложенности
-class Nesting_2(models.Model):
-    name = models.CharField(verbose_name="Наименование второго пунка вложенности", max_length=100)
+    # Настройка отображения наименования таблицы в админке
+    class Meta:
+        verbose_name = "Меню"
+        verbose_name_plural = "Меню"
 
+
+# Элементы меню
+class Elements(models.Model):
+    menu_id = models.ForeignKey(
+        "Menus", verbose_name="id меню", on_delete=models.CASCADE
+    )
+    name = models.CharField(
+        verbose_name="Наименование превого пунка вложенности", max_length=100
+    )
+    parent = models.ForeignKey(
+        "Elements", verbose_name="id родителя", on_delete=models.CASCADE, blank=True
+    )
+
+    # Настройка отображения записи в админке
     def __str__(self):
         return self.name
 
-# Третий уровень вложенности
-class Nesting_3(models.Model):
-    name = models.CharField(verbose_name="Наименование 3 пунка вложенности", max_length=100)
-
-    def __str__(self):
-        return self.name
+    # Настройка отображения наименования таблицы в админке
+    class Meta:
+        verbose_name = "Элементы меню"
+        verbose_name_plural = "Элементы меню"
