0919e314c666aeede6b27bdf93ecb98406450018
KorepanovA <al1working@mail.ru>
2024-03-24 03:43:24 +0300

    Добавлено отображение переработанных моделей в админке

--- MenuTree/mainapp/admin.py
+++ MenuTree/mainapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Menus
+from .models import Elements, Menus
 
+# Добавляем имеющиеся таблицы в админку
 admin.site.register(Menus)
+admin.site.register(Elements)
